af5bf3db6d5b1681a34a88e515af0eca5e43805f
shahriaarrr <shahriaarrr@gmail.com>
2025-05-29 12:55:48 +0330

    save selected theme

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum AppThemeMode { system, light, dark }
 
 class ThemeNotifier extends StateNotifier<AppThemeMode> {
-  ThemeNotifier() : super(AppThemeMode.system);
+  static const _themeKey = 'app_theme_mode';
 
-  void setTheme(AppThemeMode themeMode) {
+  ThemeNotifier() : super(AppThemeMode.system) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt(_themeKey);
+    if (themeIndex != null &&
+        themeIndex >= 0 &&
+        themeIndex < AppThemeMode.values.length) {
+      state = AppThemeMode.values[themeIndex];
+    }
+  }
+
+  Future<void> setTheme(AppThemeMode themeMode) async {
     state = themeMode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themeKey, themeMode.index);
   }
 
   ThemeMode get themeMode {
