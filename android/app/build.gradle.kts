814076c849710e04b302b3e71dee24ae03abd77c
Shahriar Ghasempour <75499598+shahriaarrr@users.noreply.github.com>
2025-06-05 12:01:01 +0330

    change namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tomodoro"
+    namespace = "ir.shahriaarrr.tomodoro"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
